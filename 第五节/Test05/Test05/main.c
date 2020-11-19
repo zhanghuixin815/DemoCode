0dee03b6826e0f8687b8af5ea54479db50fa8d09
zhanghuixin <huixin.zhang815@gmail.com>
2020-11-20 00:27:34 +0800

    双向链表创建 打印

--- 第五节/Test05/Test05/main.c
+++ 第五节/Test05/Test05/main.c
@@ -1,14 +1,58 @@
-//
-//  main.c
-//  Test05
-//
-//  Created by huixin.a.zhang on 2020/11/19.
-//
-
 #include <stdio.h>
+#include <stdlib.h>
+#define ERROR 0
+#define OK 1
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
+typedef int Elemtype;
+typedef int Status;
+typedef struct Node{
+    Elemtype data;
+    struct Node *prior;
+    struct Node *next;
+}Node;
+typedef struct Node * LinkList;
+
+Status CreateList(LinkList *L){
+    LinkList p,temp;
+    *L = (LinkList)malloc(sizeof(Node));
+    p = *L;
+    if (p == NULL) return ERROR;
+    p->prior = NULL;
+    p->next = NULL;
+    p->data = -1;
+
+    for (int i = 0;i < 10;i++){
+        //创建临时节点temp
+        temp = (LinkList)malloc(sizeof(Node));
+        if (temp == NULL) return ERROR;
+        temp->prior = NULL;
+        temp->next = NULL;
+        temp->data = i;
+        //把temp放入链表内
+        p->next = temp;
+        temp->prior = p;
+        p = p->next;
+    }
+    return OK;
+}
+
+void DisplayList(LinkList L){
+    LinkList  temp;
+    temp = L->next;
+    if (temp == NULL){
+        printf("双向链表为空");
+        return;
+    }
+    while (temp){
+        printf("%d   ",temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+}
+int main() {
     printf("Hello, World!\n");
+    LinkList L;
+    CreateList(&L);
+    DisplayList(L);
     return 0;
 }
